adfd0fd949fcbbcadc049e5659cc2a523db65ad0
Manuel Paz Robles <manuelpazrobles@gmail.com>
2018-10-18 19:29:41 -0500

    Se agrega un ejemplo mas de interface delegation incluyendo la clase PrintingFishAction que toma un parametro, una cadena de la comida e implementa FishAction y se sobreescribe el metodo eat y en vez de imprimir a una cadena fija imprimiremos cualquier cadena que pasemos.

--- src/Interfaces/AquariumFish.kt
+++ src/Interfaces/AquariumFish.kt
@@ -1,11 +1,8 @@
 package Interfaces
-//Por supuesto que hay diferentes colores de Plecostomi (pez gatos) en el mundo así que podemos hacer el objeto FishColor un parametro de constructor
-//con un valor por defecto igual a GoldColor y refernciar las llamamadas a la propiedad color o cualquier FishColor que le pasemos **
-class Plecostomus(fishColor: FishColor = GoldColor): FishAction, FishColor by GoldColor{
-   override fun eat() {
-        println("munch on algae")
-    }
-}
+// ****
+class Plecostomus(fishColor: FishColor = GoldColor): FishAction by PrintingFishAction("A lot of algae"), FishColor by GoldColor
+
+//Ahora Plecostomus no tiene cuerpo todos sus overrides son manejados por interface delegation
 
 interface FishAction{
     fun eat()
@@ -27,8 +24,20 @@ object GoldColor: FishColor{
     override val color = "gold"
 }
 
-
-// ** Por ejemplo si pasaramos un color rojo entonces el FishColor sería by RedColor y retorna red
 object RedColor: FishColor{
     override val color = "red"
 }
+
+//Hagamos un ejemplo mas de interface delegation. Hasta aquí el metodo eat lo que hace hasta aquí es solo imprimir lo que come así que podemos
+//hacer una clase PrintingFishAction  este toma una comida como parametro e implementa FishAction sobreescribiremos (override) eat y en vez de
+//imprimir a una cadena fija imprimiremos cualquier cadena que le pasemos
+
+class PrintingFishAction(val food: String): FishAction{
+    override fun eat() {
+        println(food)
+    }
+}
+
+//Ya que tenemos una variable miembro food no podemos imprimir un objeto FishAction queremos una instancia diferente de cada comida que se le
+//pase y entonces en Plecostomus podemos reemplazar el metodo sobreescrito eat con de delegation a FishAction y tampoco se necesitan las llaves {}
+// ****
